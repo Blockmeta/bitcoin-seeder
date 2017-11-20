7183a1690b994adbe598f0a0e6a2df5ef90f25fb
Peter Bushnell <bushsolo@gmail.com>
2017-11-20 16:37:43 +0000

    Port to HTMLCOIN2

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x1f, 0x2e, 0x3d, 0x4c };
 
 CMessageHeader::CMessageHeader()
 {
